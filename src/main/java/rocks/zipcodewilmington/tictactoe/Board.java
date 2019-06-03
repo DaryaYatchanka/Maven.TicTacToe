687abaf8c305569dd781cdf23c0e39604246caf3
Darya <Darya.yatchanka@gmail.com>
2019-06-03 17:21:42 -0400

    TicTacToe

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,77 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    private Character[][] pole;
+
+
+
     public Board(Character[][] matrix) {
+        this.pole = matrix;
+
+    }
+    public Boolean isInFavorOfLetter(char letter){
+        int countDiag1 = 0;
+        int countDiag2=0;
+        int countHor = 0;
+        int countVert = 0;
+        for (int i =0; i<pole.length; i++){
+            for(int j =0; j<pole.length; j++){
+
+                if (i==j&&pole[i][j]==letter){
+                    countDiag1 +=1;
+                }
+                if (i+j==2 && pole[i][j]==letter){
+                    countDiag2+=1;
+                }
+                if (pole[i][j]==letter){
+                    countHor+=1;
+                }
+                if (pole[j][i]==letter){
+                    countVert+=1;
+                }
+            }
+            if (countHor ==3||countVert==3){
+                return true;
+            }
+            else{
+                countHor =0;
+                countVert = 0;
+            }
+        }
+        if (countDiag1 ==3||countDiag2==3){
+            return true;
+        }
+
+        return false;
+
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        return isInFavorOfLetter('X');
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        return isInFavorOfLetter('O');
     }
 
     public Boolean isTie() {
-        return null;
+        if (isInFavorOfX()==isInFavorOfO()) {
+            return true;
+        }
+        return false;
     }
 
     public String getWinner() {
-        return null;
+        if (isTie()){
+            return "";
+        }
+        else if(isInFavorOfX()) {
+            return "X";
+        }
+        else{
+            return "O";
+        }
+
     }
 
 }
